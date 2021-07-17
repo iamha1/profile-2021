430bebafbc9b8314f8fc96d589956e9ea166c810
Ha Nguyen <nguyenthingocha2@gmail.com>
2021-07-17 13:17:02 -0400

    Create Contact.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Scrollspy from 'react-scrollspy'
-import Navbar from './Navbar';
+import  Navbar from './Navbar';
 import Header from './Header';
 import About from './About';
 import Portfolio from './Portfolio';
+import Contact from './Contact';
+
 
 function App() {
   return (
     <div className="App">
       <div>
-      <div>
-      <section id="section-1">
-        <Navbar/>
-        <Header id="masthead"/>
-      </section>
-      <section id="section-2"><About /></section>
-      <section id="section-3"><Portfolio /></section>
-      </div>
-      <Scrollspy items={ ['section-1', 'section-2', 'section-3'] } currentClassName="is-current">
-        <li><a href="#section-1">section 1</a></li>
-        <li><a href="#section-2">section 2</a></li>
-        <li><a href="#section-3">section 3</a></li>
+      <Navbar/>
+      <Scrollspy items={ ['home', 'section-1', 'section-2', 'section-3'] } currentClassName="is-current">
+        <a href="#home"><Header/></a>
+        <a href="#section-1"><About/></a>
+        <a href="#section-2"><Portfolio/></a>
+        <a href="#section-3"><Contact/></a>
       </Scrollspy>
     </div>
     </div>
