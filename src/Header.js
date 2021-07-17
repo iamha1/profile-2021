afe395c21d7730385de4f0f48643f8e9d0de9af8
Ha Nguyen <nguyenthingocha2@gmail.com>
2021-07-17 13:30:46 -0400

    create navbar.css

--- src/Header.js
+++ src/Header.js
@@ -2,14 +2,16 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <header className="masthead">
+        <div id="home" className="top-picture">
+                <header>
             <div className="container">
                 <div className="masthead-subheading">Welcome To Our Studio!</div>
                 <div className="masthead-heading text-uppercase">It's Nice To Meet You</div>
                 <a className="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a>
             </div>
-        </header>
+            </header>
+        </div>
     )
 }
 
-export default Header;
+export default Header
