3adfdffa2987d0a5dd56e866d93f6b40ab50f439
Ha Nguyen <nguyenthingocha2@gmail.com>
2021-07-20 16:49:23 -0400

    adding info for projects

--- src/data.js
+++ src/data.js
@@ -1,41 +1,86 @@
 const data = [
     {
         id: 1,
-        title: 'Travel Project',
-        image:
-        'images/travel-new-font-1.png',
+        title: 'Password Generator',
+        image: 'src/assets/img/project-img/password-generator-project.jpg',
         description:
-        'This project was written in basic HTML and CSS. However, it was the first big project made in the SheCodes Workshop set. This was a great first project that used everything we learned in HTML and CSS from the beginning.',
+            'An application that generates a random password based on user-selected criteria. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code.',
         classRow: 'row mb-5',
     },
     {
         id: 2,
-        title : 'Weather Project',
+        title: 'Curb Your Enthusiasm Quiz',
         image:
-        'images/weather-vanilla.png',
+            'src/assets/img/project-img/cye-project.jpg',
         description:
-        'This project was programmed initially with HTML, CSS and Vanilla JavaScript in the second SheCodes workshop. In the third SheCodes workshop, we upgraded the project to utilize React.js instead of regular JavaScript. I showcase both versions in the "Work" tab.',
+            'This is an application that generates timed-code quiz with multiple-choice questions. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. Technologies used: JavaScript, Moment.js, HTML, CSS, Bootstrap.',
         classRow: 'row mb-5 flex-row-reverse',
     },
     {
         id: 3,
-        title: 'Dictionary Project',
+        title: 'My Calendar',
         image:
-        'images/dictionary-2.png',
+            'src/assets/img/project-img/ha-calendar.jpg',
         description:
-        'This project was written using React as well as combining two external APIs. It was a fun project to work on since it gave a bit of extra knowledge with how to work within components (components inside components using props).',
+            'This is a simple calendar application I created for myself so that I could keep track of my schedule, meetings and event at work. My calendar represents my 9-5 working hour and it helps me to manage my time effectively. Technologies used: Moment.js, JavaScript, Bootstrap, HTLM, CSS',
         classRow: 'row mb-5',
     },
     {
         id: 4,
-        title: 'Czech K-9 Website',
+        title: 'Burger for Life!',
         image:
-        'images/czech-k-9.png',
+            'src/assets/img/project-img/burger1.jpg',
         description:
-        'This project was written using HTML, CSS, and JS. This was a project for a client that wanted to transform his website into either wordpress or a site written that was responsive. The goal was to transfer his website from a simple Wix website into something that was modern with proper English and had SEO handled.',
+            'Burger for Life! is a restaurant app that lets users put in the names of burgers they would like to eat as well as create new ones. Technologies used: MySQL, Node, Express, Handlebars.',
         classRow: 'row mb-5 flex-row-reverse',
     },
-  ];
-  
-  export default data;
+
+    {
+        id: 5,
+        title: 'SharkFin',
+        image:
+            'src/assets/img/project-img/sharkFin1.jpg',
+        description:
+            'sharkFin is a website application that allows the user to search and track stocks based on their company name! The user can then add the specific stock and choose the amount of shares to add to their portfolio. Technologies used: MaterializeCSS, JavaScript, Chart.js, API',
+        classRow: 'row mb-5 flex-row-reverse',
+    },
+    {
+        id: 6,
+        title: 'CollabHub',
+        image:
+            'src/assets/img/project-img/collabhub1.jpg',
+        description:
+            'This is an application that connects professionals with similar interests and skill sets so that they can work together, discover new opportunities and grow their network. We are inspired by the unfortunate global pandemic (Covid-19), where many people are in lockdown mode and still have the need of connecting and working with other humans. Technologies used: Node.js, MySQL, Express, Handlebars, MaterializeCSS',
+        classRow: 'row mb-5 flex-row-reverse',
+    },
+    {
+        id: 7,
+        title: 'Travel with me',
+        image:
+            'src/assets/img/project-img/travelwithme.jpg',
+        description:
+            'This is an application that allows user to search for a place of interest to travel to. It also provides information, activities for that location and you can save them for future reference. Technologies used: Material UI, React, MongoDB, Passport.js.',
+        classRow: 'row mb-5 flex-row-reverse',
+    },
+    {
+        id: 8,
+        title: 'Shakr',
+        image:
+            'src/assets/img/project-img/sharkFin1.jpg',
+        description:
+            'A full stack application resembling Tinder but for restaurants where users can obtain information for different restaurants based on locations and cuisines. | Technologies used: Java, Spring Boot Vue.js, API, PostgreSQL',
+        classRow: 'row mb-5 flex-row-reverse',
+    },
+    {
+        id: 9,
+        title: 'Full Belly',
+        image:
+            'src/assets/img/project-img/sharkFin1.jpg',
+        description:
+            'A full stack application allows users create their own libraries of recipes and create meals based on existing ingredients. | Technologies used: Java, Spring Boot, Thymeleaf, MySQL, API ',
+        classRow: 'row mb-5 flex-row-reverse',
+    },
+];
+
+export default data;
 
