11fe93f1107b897fcc1807da6a670a4e4e89dcac
Ha Nguyen <nguyenthingocha2@gmail.com>
2021-07-20 16:09:47 -0400

    header font

--- src/Header.js
+++ src/Header.js
@@ -4,10 +4,10 @@ const Header = () => {
     return (
         <div id="home" className="top-picture">
                 <header>
-            <div className="container">
+            <div className="container" id="header-space">
                 <div className="masthead-subheading">Welcome To My Humble Nest!</div>
                 <div className="masthead-heading text-uppercase">It's Nice To Meet You</div>
-                <a className="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a>
+                <a className="btn btn-primary btn-xl text-uppercase" href="#about"><span className="about-me-size">About Me</span></a>
             </div>
             </header>
         </div>
